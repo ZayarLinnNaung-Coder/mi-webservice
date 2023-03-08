9846ee89f3e92569a9bd0f8e71b32be706068903
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-03-08 09:08:50 +0630

    feat

---
+++ admin-server/src/main/java/org/ace/insurance/adminserver/security/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package org.ace.insurance.adminserver.security.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.UUID;
+
+@Configuration
+public class SecurityConfig {
+    private final AdminServerProperties adminServer;
+    private final SecurityProperties security;
+
+    public SecurityConfig(AdminServerProperties adminServer, SecurityProperties security) {
+        this.adminServer = adminServer;
+        this.security = security;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
+
+        return http.authorizeHttpRequests(
+                authReq -> authReq
+                            .requestMatchers(adminServer.path("/assets/**")).permitAll()
+                            .requestMatchers(adminServer.path("/actuator/info")).permitAll()
+                            .requestMatchers(adminServer.path("/actuator/health")).permitAll()
+                            .requestMatchers(adminServer.path("/login")).permitAll()
+                            .anyRequest().authenticated()
+        ).formLogin(
+                (formLogin) ->
+                        formLogin.loginPage(adminServer.path("/login")).successHandler(successHandler)
+                                .and()
+        ).logout((logout) -> logout.logoutUrl(adminServer.path("/logout"))).httpBasic(Customizer.withDefaults())
+                .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers(
+                                new AntPathRequestMatcher(this.adminServer.path("/instances"),
+                                        HttpMethod.POST.toString()),
+                                new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
+                                        HttpMethod.DELETE.toString()),
+                                new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
+                        ))
+                .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600)).build();
+    }
+}
